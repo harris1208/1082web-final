263a650b43ae52d9de53174fad651cdf36782605
harris1208 <51354020+harris1208@users.noreply.github.com>
2020-06-06 09:16:41 +0800

    Update main.js

--- club-course/main.js
+++ club-course/main.js
@@ -6,6 +6,11 @@ $(document).ready(function(){
     );
 
     $("#date-button").click(function(){
+        $("#courseTable").empty();
+
+    $("#courseTable").append(
+        "<tr><th>日期</th><th>假期</th></tr>"
+    );
         let StartDate = $("#StartDate").val();
         let EndDate = $("#EndDate").val();
         if(StartDate==""){
